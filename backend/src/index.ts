51c5d48e35390961697a441554d8a9950229e965
Anthony <anthonysalisbury98@outlook.com>
2024-08-20 22:11:17 +0100

    Set up backend

---
+++ backend/src/index.ts
@@ -0,0 +1,26 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import cors from 'cors';
+import mongoose from 'mongoose';
+import 'dotenv/config'
+
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+mongoose.set("strictQuery", false);
+const mongoDB: string | any = process.env.MONGO_URL;
+
+main().catch((err) => console.log(err));
+async function main() {
+  await mongoose.connect(mongoDB);
+  console.log("MongoDB connected");
+}
+
+app.use(cors());
+app.use(bodyParser.json());
+
+app.listen(PORT, () => {
+    console.log(`Server running on port: http://localhost:${PORT}/`);
+});
+
